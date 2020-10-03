3e22ba6e067865227bb4ece97e1e7f08b6f76229
izzatjff <62347288+izzatjff@users.noreply.github.com>
2020-10-03 23:39:55 +0800

    Add user information table into database

---
+++ foodedge.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 03, 2020 at 05:36 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `foodedge`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userinformation`
+--
+
+CREATE TABLE `userinformation` (
+  `CustomerID` int(11) NOT NULL,
+  `CustomerFName` varchar(255) NOT NULL,
+  `CustomerLName` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` char(82) NOT NULL,
+  `PhoneNo` varchar(15) NOT NULL,
+  `SecQuestion` varchar(255) NOT NULL,
+  `SecAnswer` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `userinformation`
+--
+
+INSERT INTO `userinformation` (`CustomerID`, `CustomerFName`, `CustomerLName`, `Email`, `Password`, `PhoneNo`, `SecQuestion`, `SecAnswer`) VALUES
+(1, 'Simon', 'Jingga', 'aoidja@gmail.com', 'oawjdoiawd', '0129301', 'oaksdoak', 'jingger'),
+(2, 'Jibril', 'Saleem', 'jibril@gmail.com', 'presefy123', '0123456789', 'whats ur cat name', 'jingcat');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `userinformation`
+--
+ALTER TABLE `userinformation`
+  ADD PRIMARY KEY (`CustomerID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `userinformation`
+--
+ALTER TABLE `userinformation`
+  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
